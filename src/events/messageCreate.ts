d9bb56183255df03cf7325309af0712fab5b798a
Vuxxs <57200277+Vuxxs@users.noreply.github.com>
2023-05-18 10:11:59 +0300

    messageCreate commands logic

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -5,9 +5,29 @@ export default (client: CustomClient): void => {
   client.on("messageCreate", async (message) => {
     if (
       message.channel.type !== ChannelType.GuildText ||
-      message.author.id === client.user!.id
+      message.author.id === client.user!.id ||
+      !client.commands
     )
       return;
+
     // This is where the magic happens
+    if (!message.content.startsWith(client.options.prefix)) return;
+
+    const args = message.content
+      .slice(client.options.prefix.length)
+      .trim()
+      .split(/ +/);
+    const commandName = args.shift()?.toLowerCase();
+
+    const command = client.commands.get(commandName!);
+
+    if (!command) return;
+
+    try {
+      await command.execute(undefined, message);
+    } catch (err) {
+      console.error(`Error executing command "${commandName}":`, err);
+      message.channel.send("An error occured while executing the command.");
+    }
   });
 };
