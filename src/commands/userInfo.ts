76472f83320dce71becb452e5cdf7ee88056f603
Vuxxs <57200277+Vuxxs@users.noreply.github.com>
2023-05-22 15:02:49 +0300

    userInfo prefix command

--- src/commands/userInfo.ts
+++ src/commands/userInfo.ts
@@ -5,6 +5,7 @@ import {
   Message,
 } from "discord.js";
 import { Command } from "../interfaces/command.interface";
+import { filterUserId } from "../utilities/filterUserId";
 
 export default {
   name: "userinfo",
@@ -17,7 +18,11 @@ export default {
       required: false,
     },
   ],
-  async execute(interaction?: CommandInteraction, message?: Message) {
+  async execute(
+    interaction?: CommandInteraction,
+    message?: Message,
+    args?: String[]
+  ) {
     let member;
     let user;
     if (interaction) {
@@ -25,7 +30,10 @@ export default {
       member = interaction.guild?.members.cache.get(user.id)!;
     } else if (message) {
       user = message!.author;
-      member = message.guild?.members.cache.get(user.id)!;
+      if (args && args.length > 0)
+        user = message.guild?.members.cache.get(filterUserId(args[0]))?.user;
+      if (!user) return;
+      member = message.guild?.members.cache.get(user.id);
     } else {
       return;
     }
