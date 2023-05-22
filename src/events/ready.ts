6f0588c561dbce032f59fe9453f37b5c8dc71044
Vuxxs <57200277+Vuxxs@users.noreply.github.com>
2023-05-22 11:02:01 +0300

    Ready Event
    Additions
    - log users besides bot in activity
    - register slash commands

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,13 +1,36 @@
 import { ActivityType } from "discord.js";
 import { CustomClient } from "../interfaces/client.interface";
 import { LogLevel, angelogger } from "../utilities/logger";
+import { registerSlashCommands } from "../utilities/registerCommands";
 
 export default (client: CustomClient): void => {
   client.on("ready", async () => {
+    // Compute the total number of unique users the bot serves, excluding bots
+    const userSet = new Set();
+    client.guilds.cache.forEach((guild) => {
+      guild.members.cache.forEach((member) => {
+        if (!member.user.bot) userSet.add(member.user.id);
+      });
+    });
+    const userCount = userSet.size;
+
+    // Set the activity
     client.user?.setActivity({
       type: ActivityType.Watching,
-      name: `${client.options.prefix}help`,
+      name: `${userCount} users | ${client.options.prefix}help`,
     });
-    angelogger(LogLevel.INFO, `${client.user!.username} is online`);
+
+    // Register slash commands
+
+    const clientId = client.user?.id;
+    if (clientId) {
+      await registerSlashCommands(client, clientId);
+    }
+
+    // Log that the bot is online
+    angelogger(
+      LogLevel.INFO,
+      `${client.user!.username} is online and serving ${userCount} users.`
+    );
   });
 };
