62cf1a021a722edde9cd45e277a19399cf2db2fb
Vuxxs <57200277+Vuxxs@users.noreply.github.com>
2023-05-26 02:45:04 +0300

    ping
    - removed DRY code

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,14 +1,11 @@
 import { CommandInteraction, Message } from "discord.js";
 import { Command } from "../interfaces/command.interface";
+import { sendMessage } from "../utilities/sendMessage";
 
 export default {
   name: "ping",
   description: "Ping command",
   execute: async (interaction?: CommandInteraction, message?: Message) => {
-    if (interaction && interaction.isCommand()) {
-      await interaction.reply("Pong!");
-    } else if (message) {
-      message.channel.send("Pong!");
-    }
+    sendMessage(message, interaction, "Pong!");
   },
 } as Command;
