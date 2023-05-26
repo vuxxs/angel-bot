121389a0b284e48aa25566339cce6a8967bec996
Vuxxs <57200277+Vuxxs@users.noreply.github.com>
2023-05-26 08:10:28 +0300

    joke Command

---
+++ src/commands/joke.ts
@@ -0,0 +1,22 @@
+import { CommandInteraction, Message } from "discord.js";
+import { Command } from "../interfaces/command.interface";
+import { sendMessage } from "../utilities/sendMessage";
+
+// Move & pull from database in the future
+const jokes = [
+  "Why don't scientists trust atoms? Because they make up everything!",
+  "Why did the chicken go to the séance? To talk to the other side.",
+  "Why don't some animals play cards? Because they're afraid of cheetahs.",
+  "What do you call cheese that isn't yours? Nacho cheese!",
+  "Why couldn't the bicycle stand up by itself? It was two tired!",
+];
+
+export default {
+  name: "joke",
+  description: "Tells a random joke",
+  async execute(interaction?: CommandInteraction, message?: Message) {
+    const joke = jokes[Math.floor(Math.random() * jokes.length)];
+
+    sendMessage(message, interaction, joke);
+  },
+} as Command;
